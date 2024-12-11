eb58660a779e3a64a8628ff1e10fcc3747a7d045
thurtado <tony-hurtado@comcast.net>
2024-12-10 21:02:23 -0800

    Initial Commit

---
+++ TonyzTest/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TonyzTest
+//
+//  Created by Tony Hurtado on 12/10/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
